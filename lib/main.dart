89f91c387280df7bbd6fcfde7a928d3eb5020990
AbuD2023 <zooro7709@gmail.com>
2025-12-01 22:57:50 +0300

    feat: Integrate Firebase authentication and core services

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_delivery/core/constants/app_them.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'features/splash/presentaion/page/splash_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ProviderScope(child: const MyApp()));
 }
 
